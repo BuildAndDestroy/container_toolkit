b56ee07f15f9a7119711d6085b8e987693fd6565
BuildAndDestroy <devreap1@gmail.com>
2020-05-10 23:29:09 -0600

    k3s with and without Docker now works

--- raspberry_pi/rpi_container_toolkit.sh
+++ raspberry_pi/rpi_container_toolkit.sh
@@ -16,14 +16,26 @@ function _help_menu() {
     # Help menu
     echo '[*] Help Menu:'
     echo ''
-    echo '[*] --init       Run first on every pi! Then move on to --master OR --worker.'
+    echo '[*] --init       Run first on every pi! Then move on to --master OR --worker. TURNED OFF FOR NOW.'
     echo '                      bash rpi_container_toolkit.sh --init'
     echo ''
-    echo '[*] --master     Install Kubernetes MASTER NODE Calico and Docker.'
+    echo '[*] --master     Install full Kubernetes k8s MASTER NODE Calico and Docker. DOES NOT WORK'
     echo '                      bash rpi_container_toolkit.sh --master'
     echo ''
-    echo '[*] --worker     Install Kubernetes WORKER NODE and Docker'
+    echo '[*] --worker     Install full Kubernetes k8s WORKER NODE and Docker. DOES NOT WORK'
     echo '                      bash rpi_container_toolkit.sh --worker'
+    echo ''
+    echo '[*] --rancherk3s-master    The generic k3s install for Master.'
+    echo '                     bash rpi_container_toolkit.sh --rancherk3s-master'
+    echo ''
+    echo '[*] --rancherk3s-worker    The generic k3s install for Worker.'
+    echo '                     bash rpi_container_toolkit.sh --rancherk3s-worker'
+    echo ''
+    echo '[*] --rancherk3s-master-docker    K3s install for Master but with Docker instead of containerd.'
+    echo '                     bash rpi_container_toolkit.sh --rancherk3s-master-docker'
+    echo ''
+    echo '[*] --rancherk3s-worker-docker    K3s install for Worker but with Docker instead of containerd.'
+    echo '                     bash rpi_container_toolkit.sh --rancherk3s-worker-docker'
     echo ''
     echo '[*] --helm       Install Helm3 to Master'
     echo '                      bash rpi_container_toolkit.sh --helm'
@@ -104,32 +116,32 @@ function install_kubernetes() { #  Install Kubernetes
     /usr/bin/sudo apt-get -y install kubelet kubectl kubeadm
 }
 
-function set_cgroup_driver() {
-    # Update cgroup driver for kubernetes
-    echo '[*] Verify Docker cgroupfs'
-    docker info | grep -i cgroup
-    echo '[*] Updating Docker to use same cgroup.'
-cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2",
-  "storage-opts": [
-    "overlay2.override_kernel_check=true"
-  ]
-}
-EOF
-    mkdir -p /etc/systemd/system/docker.service.d
-    systemctl daemon-reload
-    systemctl restart docker
-    systemctl restart kubelet
-}
+# function set_cgroup_driver() {
+#     # Update cgroup driver for kubernetes
+#     echo '[*] Verify Docker cgroupfs'
+#     docker info | grep -i cgroup
+#     echo '[*] Updating Docker to use same cgroup.'
+# cat > /etc/docker/daemon.json <<EOF
+# {
+#   "exec-opts": ["native.cgroupdriver=systemd"],
+#   "log-driver": "json-file",
+#   "log-opts": {
+#     "max-size": "100m"
+#   },
+#   "storage-driver": "overlay2",
+#   "storage-opts": [
+#     "overlay2.override_kernel_check=true"
+#   ]
+# }
+# EOF
+#     mkdir -p /etc/systemd/system/docker.service.d
+#     systemctl daemon-reload
+#     systemctl restart docker
+#     systemctl restart kubelet
+# }
 
 function cgroup_to_bootfile() { #  Must use cgroup memory, needed in boot file
-    sed -i 's/$/\ cgroup_enable=cpuset\ cgroup_enable=memory/' /boot/firmware/nobtcmd.txt
+    sed -i 's/$/\ cgroup_enable=cpuset\ cgroup_memory=1\ cgroup_enable=memory/' /boot/firmware/nobtcmd.txt
 }
 
 function reboot_the_pi() {
@@ -174,6 +186,38 @@ function join_worker_node() {
     echo '        kubeadm token create --print-join-command'
 }
 
+#############
+# k3s Master
+
+function k3s_master_kubernetes() {
+    curl -sfL https://get.k3s.io | sh -
+    /bin/cat /var/lib/rancher/k3s/server/node-token
+}
+
+function k3s_systemctl_docker() {
+    /usr/local/bin/k3s-killall.sh
+    /bin/sed -i 's@server\ \@@g' /etc/systemd/system/k3s.service
+    /bin/sed -i 's@ExecStart=/usr/local/bin/k3s\ \@ExecStart=/usr/local/bin/k3s\ server\ --docker@g' /etc/systemd/system/k3s.service
+    systemctl daemon-reload
+    systemctl stop k3s
+    systemctl start k3s
+}
+
+#############
+# k3s Worker
+
+function k3s_worker_kubernetes() {
+    /bin/echo '[*] Type in the Master token file from /var/lib/rancher/k3s/server/node-token'
+    read -r -p "Node Token: " k3s_token
+
+    /bin/echo '[*] Type in the Master nodes IP Address.'
+    read -r -p "Master's IP Address: " master_ip
+
+    export K3S_URL="https://"$master_ip":6443"
+    export K3S_TOKEN=""$k3s_token""
+    curl -sfL https://get.k3s.io | sh -
+}
+
 #################
 #  Helm charts  #
 #################
@@ -203,25 +247,55 @@ function isntall_traefik_loadbalancer() { #  Install a loadbalancer for pi.
 
 case "$1" in
     --init)
-        run_as_root
-        update_apt
-        enable_traffic_forwarding
-        disable_swap
-        install_docker
-        ufw_ports_allowed
-        install_kubernetes
-        set_cgroup_driver
-        cgroup_to_bootfile
-        reboot_the_pi
+        echo 'Pending the full K8s build.'
+        # run_as_root
+        # update_apt
+        # enable_traffic_forwarding
+        # disable_swap
+        # install_docker
+        # ufw_ports_allowed
+        # install_kubernetes
+        # set_cgroup_driver
+        # cgroup_to_bootfile
+        # reboot_the_pi
         ;;
     --master)
-        kubernetes_api_server
-        install_calico_network_policy
-        install_calicoctl
+        # kubernetes_api_server
+        # install_calico_network_policy
+        # install_calicoctl
         # If no join command, use "kubeadm token create --print-join-command"
+        echo 'Still testing, full k8s not working'
         ;;
     --worker)
-        join_worker_node
+        # join_worker_node
+        echo 'Still testing, full k8s not working'
+        ;;
+    --rancherk3s-master)
+        cgroup_to_bootfile
+        ufw_ports_allowed
+        k3s_master_kubernetes
+        reboot_the_pi
+        ;;
+    --rancherk3s-worker)
+        cgroup_to_bootfile
+        ufw_ports_allowed
+        k3s_worker_kubernetes
+        reboot_the_pi
+        ;;
+    --rancherk3s-master-docker)
+        cgroup_to_bootfile
+        install_docker
+        ufw_ports_allowed
+        k3s_master_kubernetes
+        k3s_systemctl_docker
+        reboot_the_pi
+        ;;
+    --rancherk3s-worker-docker)
+        cgroup_to_bootfile
+        install_docker
+        ufw_ports_allowed
+        k3s_worker_kubernetes
+        reboot_the_pi
         ;;
     --helm)
         run_as_root
