8cfc194987635594bf5a234d9c1946749ece700c
BuildAndDestroy <devreap1@gmail.com>
2022-01-26 01:00:38 -0700

    Helm3: Update helm3 from package manager

--- Ubuntu/container_toolkit.sh
+++ Ubuntu/container_toolkit.sh
@@ -237,11 +237,11 @@ function install_helm_chart() {
 }
 
 function install_helm_three(){
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-    chmod 700 get_helm.sh
-    ./get_helm.sh
-    helm repo add stable https://charts.helm.sh/stable
-    helm repo update
+    curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+    sudo apt-get install apt-transport-https --yes
+    echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+    sudo apt-get update
+    sudo apt-get install helm
 }
 
 function install_pure_storage_pso() { #  Install the Pure Storage Orchestrator for Kubernetes.
